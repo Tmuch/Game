453c515eb55a57d3ef8e60fd8b17054ac2495526
Tyler Much <tyler.much@marquette.edu>
2013-08-10 12:32:48 -0500

    Game.java : add render(), update(), and Display init

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,10 +1,19 @@
 package main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Game {
 	
 	private final long UPDATES_PER_SEC = 60;
 	private final long SECOND_NS = 1000000000L;
 	private final long SECOND_MS = 1000L;
+	
+	private final int WIDTH = 800;
+	private final int HEIGHT = 600;
 	
 	
 	private long fps;	// frames per second
@@ -46,36 +55,66 @@ public class Game {
 	private void start()
 	{
 		if(running) return;
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create();
+			Keyboard.create();
+			Mouse.create();
+			Display.setTitle("Game");
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		
 		run();
 	}
 	
 	private void run()
 	{
-		System.out.println(Long.MAX_VALUE);
 		running = true;
 		while(running)
 		{
-			currTime = System.nanoTime();
-			delta += (currTime - lastTime) / (SECOND_NS / (double)UPDATES_PER_SEC);
-			lastTime = currTime;
 			
-			while(delta >= 1)
+			if(Display.isCloseRequested()) stop();
+			if(paused)
 			{
-				//System.out.println("update");
-				updates++;
-				delta--;
-			}
-			
-			currTime = System.currentTimeMillis();
-			elapsed += currTime - prevTimeMillis;
-			prevTimeMillis = currTime;
-			ups = (updates * SECOND_MS) / elapsed;
-			System.out.println(ups);
-			
-			if(elapsed > Long.MAX_VALUE / 2)
+				
+			} else
 			{
-				elapsed = 1;
-				updates = 0;
+			
+				currTime = System.nanoTime();
+				delta += (currTime - lastTime) / (SECOND_NS / (double)UPDATES_PER_SEC);
+				lastTime = currTime;
+				
+				while(delta >= 1)
+				{
+					//world.update();
+					updates++;
+					delta--;
+				}
+				
+				
+				input();
+				update();
+				render();
+				//frames++;
+				
+				currTime = System.currentTimeMillis();
+				elapsed += currTime - prevTimeMillis;
+				prevTimeMillis = currTime;
+				ups = (updates * SECOND_MS) / elapsed;
+				//fps = (frames * SECOND_MS) / elapsed;
+				//System.out.println(ups);
+				//System.out.println(fps);
+				
+				if(elapsed > Long.MAX_VALUE / 2)
+				{
+					elapsed = 1;
+					updates = 0;
+				}
+			
 			}
 		}
 		
@@ -90,11 +129,27 @@ public class Game {
 	
 	private void cleanup()
 	{
-		
+		Display.destroy();
+		Keyboard.destroy();
+		Mouse.destroy();
+	}
+	
+	private void render()
+	{
+		//world.render();
+		Display.update();
 	}
 	
 	
+	private void input()
+	{
+		
+	}
 	
+	private void update()
+	{
+		
+	}
 	
 
 }
